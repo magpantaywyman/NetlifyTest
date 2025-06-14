49b745315b2eb741a8fc3445a6691857bd9f5a79
magpantaywyman <magpantaywyman@gmail.com>
2025-06-14 18:12:50 +0800

    Time function troubleshooting

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -1,6 +1,6 @@
-export async function handler() {
+exports.handler = async function () {
   return {
     statusCode: 200,
     body: JSON.stringify({ time: new Date().toISOString() }),
   };
-}
+};
